01242d9f17b2716a6df0d0a75a33330dfd2f744d
Kappa6103 <schadekevin@protonmail.com>
2025-08-21 13:59:12 +0200

    spring boot project loaded in the repo

---
+++ src/main/java/com/paymybuddy/PayementWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayementWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayementWebAppApplication.class, args);
+	}
+
+}
