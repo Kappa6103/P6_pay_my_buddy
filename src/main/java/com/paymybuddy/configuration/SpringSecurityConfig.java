2f4df45da583d868b8b29b807c22d3d9bfbd5d92
Kappa6103 <schadekevin@protonmail.com>
2025-08-26 15:27:10 +0200

    Spring Security config class added, routing incoming connexion to /connexion

---
+++ src/main/java/com/paymybuddy/configuration/SpringSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.paymybuddy.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/connexion").permitAll()
+                        .requestMatchers("/inscription").permitAll()
+                        .requestMatchers("/ajouter_relation").authenticated()
+                        .requestMatchers("/profil").authenticated()
+                        .requestMatchers("/transfert").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/profil", true)
+                )
+                .logout(config -> config.logoutSuccessUrl("/connexion"))
+                .build();
+    }
+
+}
