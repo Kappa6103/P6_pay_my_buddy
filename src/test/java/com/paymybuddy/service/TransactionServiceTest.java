2ee6906e0897f56713980e1a2490975a9a9c1a6f
Kappa6103 <schadekevin@protonmail.com>
2025-09-08 14:55:31 +0200

    modifier profil fixed

---
+++ src/test/java/com/paymybuddy/service/TransactionServiceTest.java
@@ -0,0 +1,20 @@
+package com.paymybuddy.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionServiceTest {
+
+    @Test
+    void createTransaction() {
+    }
+
+    @Test
+    void processTransaction() {
+    }
+
+    @Test
+    void getTransactionHistoryDtoByUser() {
+    }
+}
